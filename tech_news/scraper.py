c204db46e868197585ce17e5fa88d349d269477d
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-22 14:49:14 -0300

    feat: Cria a função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,23 +1,36 @@
+import requests
+import time
+# import parsel
+
+
 # Requisito 1
-def fetch(url):
-    """Seu código deve vir aqui"""
+def fetch(url: str, aguarde: int = 1) -> str:
+    headers = {"user-agent": "Fake user-agent"}
+    try:
+        time.sleep(1)
+        response = requests.get(url, timeout=aguarde, headers=headers)
+        response.raise_for_status()
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
+    else:
+        return response.text
 
 
 # Requisito 2
 def scrape_updates(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    pass
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    pass
