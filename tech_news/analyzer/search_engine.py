f7dc4832623064277a8b04cada8e8030357f841a
lbseven7 <leobarbosapinturas@gmail.com>
2023-01-25 15:05:18 -0300

    feat: Cria a função search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,11 +1,38 @@
+# from typing import List, Tuple
+from tech_news.database import search_news
+# import datetime
+# from pymongo import MongoClient
+
+
 # Requisito 6
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    def pega_lista(query):
+        todas_as_noticias = search_news(query)
+        noticias_encontradas = []
+
+        # title = title.lower()
+        for noticia in todas_as_noticias:
+            noticias_encontradas.append((noticia["title"], noticia["url"]))
+
+        return noticias_encontradas
+    return pega_lista({"title": {"$regex": title, "$options": "i"}})
 
 
 # Requisito 7
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    # date = datetime.strptime(date, '%Y-%m-%d').strftime('%d/%m/%Y')
+    # client = MongoClient()
+    # db = client.news_database
+
+    # news = list(db.news_collection.find({"timestamp": date}))
+    # if not news:
+    #     return []
+
+    # return [(new['title'], new['url']) for new in news]
 
 
 # Requisito 8
